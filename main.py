6982e1b7d1db9e2f34d8ae2ef8b4c6eb955d2a06
Piotr Konkol <piotrkonkol@protonmail.com>
2022-01-20 23:45:12 +0100

    Gather links from the first page without scrolling down

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 # from tkinter import W
 from selenium import webdriver
+from pprint import pprint
 
 url = "https://graymirror.substack.com/"
 
@@ -13,4 +14,11 @@ driver.save_screenshot('screen.png')
 driver.find_element_by_xpath('//button[text()="Let me read it first"]').click()
 time.sleep(2)
 driver.save_screenshot('screen2.png')
+posts = driver.find_elements_by_class_name("post-preview")
+table = []
+for post in posts:
+    p = post.find_element_by_class_name("post-preview-title").get_attribute('href')
+    # d = post.find_element_by_class_name("post-date").get_attribute('title')
+    table.append((p, ))
+pprint(table)
 driver.quit()

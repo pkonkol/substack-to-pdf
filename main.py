24a76e78709cb4865a8cd248d31545ed8797c536
Piotr Konkol <piotrkonkol@protonmail.com>
2022-01-20 23:22:08 +0100

    First click is working!

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+# from tkinter import W
+from selenium import webdriver
 
 url = "https://graymirror.substack.com/"
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-print(response.content)
+options = webdriver.ChromeOptions();
+options.add_argument('--headless');
+options.add_argument('window-size=1920x1080');
+driver = webdriver.Chrome(options=options)
+resp = driver.get(url)
+driver.save_screenshot('screen.png')
+driver.find_element_by_xpath('//button[text()="Let me read it first"]').click()
+time.sleep(2)
+driver.save_screenshot('screen2.png')
+driver.quit()

0375d24666664d2d34e98b657a24e8bc74fe74c5
Piotr Konkol <piotrkonkol@protonmail.com>
2022-01-21 22:31:35 +0100

    Scraping all the article previews in the archive. Padded with sleeps, to improve

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pprint import pprint
 url = "https://graymirror.substack.com/"
 
 options = webdriver.ChromeOptions();
-options.add_argument('--headless');
+# options.add_argument('--headless');
 options.add_argument('window-size=1920x1080');
 driver = webdriver.Chrome(options=options)
 resp = driver.get(url)
@@ -21,4 +21,30 @@ for post in posts:
     # d = post.find_element_by_class_name("post-date").get_attribute('title')
     table.append((p, ))
 pprint(table)
+driver.get(
+    driver.find_element_by_class_name("portable-archive-all").get_attribute('href')
+)
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+    recent_height = driver.execute_script("return document.body.scrollHeight")
+    print(f"scrolling screen down. last:{last_height}, " 
+          f"now:{recent_height}")
+    if recent_height  == last_height:
+        break
+    time.sleep(2)
+    last_height = recent_height
+
+time.sleep(15)
+
+posts = driver.find_elements_by_class_name("post-preview")
+table = []
+for post in posts:
+    p = post.find_element_by_class_name("post-preview-title").get_attribute('href')
+    # d = post.find_element_by_class_name("post-date").get_attribute('title')
+    table.append((p, ))
+pprint(table)
+
+driver.save_screenshot('screen3.png')
 driver.quit()
